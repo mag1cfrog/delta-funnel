55404b862f2f38a1801635b4f57f1cef68600f21
mag1cfrog <harrywong2017@gmail.com>
2026-06-13 19:53:28 -0700

    feat(datafusion): add provider read stats

--- crates/delta-funnel/src/query_engine/datafusion/execution.rs
+++ crates/delta-funnel/src/query_engine/datafusion/execution.rs
@@ -3,6 +3,7 @@
 pub(crate) mod environment;
 pub(crate) mod file_reader;
 mod planning_exec;
+pub(crate) mod read_stats;
 pub(crate) mod scheduling;
 
 pub(crate) use planning_exec::DeltaScanPlanningExec;
